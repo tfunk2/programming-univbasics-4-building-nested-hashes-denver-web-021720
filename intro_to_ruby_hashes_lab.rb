739022e6f11c08ce1a2217ddd599ff842a7cc4f7
Tyler Funk <tyler.j.funk2@gmail.com>
2020-02-05 07:19:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,8 +24,8 @@ def monopoly_with_third_tier
     :pieces => 4,
     :rent_in_dollars => {
       one_piece_owned: 25,
-      key2: "",
-      key3: "",
+      two_pieces_owned: 50,
+      three_pieces_owned: 75,
       key4: ""
       },
     :names => {
