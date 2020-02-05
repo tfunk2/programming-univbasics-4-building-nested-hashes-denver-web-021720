035f6f3d64b18de23b3148d2877c6217d5b3ec2b
Tyler Funk <tyler.j.funk2@gmail.com>
2020-02-05 07:14:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,12 @@ def monopoly_with_third_tier
   # complexity.
   {:railroads => {
     :pieces => 4,
-    :rent_in_dollars => []
+    :rent_in_dollars => {
+      key1: "",
+      key2: "",
+      key3: "",
+      key4: ""
+      }
     }
   }
 end
